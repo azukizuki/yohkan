4ed9afc2669bc30b974fedac6890e7618436c5c0
azukiidx <rootdouble@gmail.com>
2024-03-04 17:05:02 +0900

    v0.0.6 update property change

--- Assets/yohkan/editor/YohkanAssetBundleBuilderConfig.cs
+++ Assets/yohkan/editor/YohkanAssetBundleBuilderConfig.cs
@@ -7,16 +7,31 @@ namespace yohkan.editor
     public class YohkanAssetBundleBuilderConfig : ScriptableObject
     {
         [SerializeField] private string _contentStateBinaryRootDir = string.Empty;
-        public string ContentStateBinaryRootDir => _contentStateBinaryRootDir;
+        public string ContentStateBinaryRootDir
+        {
+            get => _contentStateBinaryRootDir;
+            set => _contentStateBinaryRootDir = value;
+        }
 
         [SerializeField] private string _remoteBuildRootPath = string.Empty;
-        public string RemoteBuildRootPath => _remoteBuildRootPath;
-        
+        public string RemoteBuildRootPath
+        {
+            get => _remoteBuildRootPath;
+            set => _remoteBuildRootPath = value;
+        }
+
         [SerializeField] private string _remoteLoadRootPath = string.Empty;
-        public string RemoteLoadRootPath => _remoteLoadRootPath;
+        public string RemoteLoadRootPath
+        {
+            get => _remoteLoadRootPath;
+            set => _remoteLoadRootPath = value;
+        }
 
         [SerializeField] private bool _enableYohkanAddressableLabelSetWhenBundleBuild = true;
-        public bool EnableYohkanAddressableLabelSetWhenBundleBuild => _enableYohkanAddressableLabelSetWhenBundleBuild;
-        
+        public bool EnableYohkanAddressableLabelSetWhenBundleBuild
+        {
+            get => _enableYohkanAddressableLabelSetWhenBundleBuild;
+            set => _enableYohkanAddressableLabelSetWhenBundleBuild = value;
+        }
     }
 }
